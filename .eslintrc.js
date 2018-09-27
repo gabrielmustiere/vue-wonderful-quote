a90102688f3666b69a813e94b9d4a9562aeff0f6
gabrielmustiere <gmustiere@gemy.fr>
2018-09-27 13:34:09 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    "indent": ["error", 2, {"SwitchCase": 1}],
+    "import/no-unresolved": 0,
+    "import/no-unassigned-import": 0,
+    "block-spacing": "off",
+    "semi": "off",
+    "no-multiple-empty-lines": ["error", {"max": 1, "maxBOF": 1}],
+    "padding-line-between-statements": [
+      "error",
+      {blankLine: "always", prev: ["const", "let", "var"], next: "*"},
+      {blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"]}
+    ],
+    "no-console": "off",
+    "space-before-function-paren": [
+      "error",
+      {
+        "anonymous": "always",
+        "named": "always",
+        "asyncArrow": "always"
+      }
+    ]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
